e60fb552aef4bd6b2e448525dd8d065c2a452a3a
Nour Al Salhi <nuraym9998@gmail.com>
2026-01-20 13:28:12 +0200

    edit type var

--- types/index.ts
+++ types/index.ts
@@ -4,9 +4,26 @@ export interface InventoryItem {
   sku: string;
   quantity: number;
 }
-
 export interface Location {
   id: number;
   name: string;
-  type: 'warehouse' | 'store';
+  type: "warehouse" | "store";
+}
+export interface InventoryProduct {
+  id: number;
+  product_name: string;
+  quantity: number;
+}
+export interface TransferFormProps {
+  onTransferSuccess?: () => void;
+}
+export interface TransferFormState {
+  sourceType: "Warehouse" | "Store" | null;
+  sourceId: number | null;
+  destinationId: number | null;
+  productId: number | null;
+  quantity: number;
+  sources: Location[];
+  destinations: Location[];
+  products: InventoryProduct[];
 }
