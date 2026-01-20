cd233f5fbdbaa4272430be73e20c0c1451a7eef3
Nour Al Salhi <nuraym9998@gmail.com>
2026-01-20 11:13:39 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Inverntory Management System",
+  description: "A simple inventory management system built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
